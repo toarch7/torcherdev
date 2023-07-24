a4619d7918b5f9988b48a05c424a110f4ee8e6ca
toncho_ <69200110+toarch7@users.noreply.github.com>
2023-07-24 04:56:51 +0300

    Update update.js

--- update.js
+++ update.js
@@ -19,7 +19,7 @@ if (API_URL) {
 
             console.log(loadout);
 
-            if (!loadout.active || !loadout.char)
+            if (!loadout.active || loadout.active == "false" || !loadout.char)
                 return;
             
             let data = {
